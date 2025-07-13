86e1540a1f37a2448aadee681ece3c6181500323
Amalcs9188 <amalcs645@gmail.com>
2025-07-13 11:33:57 +0530

    * margin fix

--- src/components/site-header.jsx
+++ src/components/site-header.jsx
@@ -9,8 +9,10 @@ export function SiteHeader() {
       <div className="flex w-full items-center gap-1 px-4 lg:gap-2 lg:px-6">
         <SidebarTrigger className="-ml-1" />
         <Separator orientation="vertical" className="mx-2 data-[orientation=vertical]:h-4" />
-        <h1 className="text-base flex  items-center text-gray-300 capitalize font-bold text-center gap-1 font-geist-sans">All Your Information In One Place <ShieldPlus size={20} strokeWidth={2.75} absoluteStrokeWidth /> </h1>
-        <div className="ml-auto flex items-center gap-2">
+        <div className=" w-full flex justify-center items-center">
+          <h1 className="text-base flex  items-center  mr-4 text-gray-300 capitalize font-bold text-center gap-1 font-geist-sans">All Your Information In One Place <ShieldPlus size={20} strokeWidth={2.75} absoluteStrokeWidth /> </h1>
+
+        </div>        <div className="ml-auto flex items-center gap-2">
           <Button variant="ghost" asChild size="sm" className="hidden sm:flex">
             <a
               href="https://github.com/Amalcs9188/DocsData"
